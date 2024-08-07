5565e7809f451861c151e575c98654f8af5dfb9b
Jisshu developer <142872501+Jisshubot@users.noreply.github.com>
2024-08-07 12:50:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot SadKidBGMZ/fwdbot 
+  git clone https://github.com/Jisshubot/Jisshu-forward-bot Jisshubot/fwdbot 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot -b $BRANCH /fwdbot
+  git clone https://github.com/Jisshubot/Jisshu-forward-bot -b $BRANCH /jsfwdbot
 fi
-cd SadKidBGMZ/fwdbot 
+cd Jisshubot/jsfwdbot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 main.py
+gunicorn app:app & python3 main.py
